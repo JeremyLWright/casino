f2ce19b937ca829598e5e7a2801e6865b09cf9b7
Jeremy Wright <jeremy@quiescent.us>
2017-06-29 21:52:46 -0700

    Started Bin class.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,19 @@
 module Lib
     ( someFunc,
       Outcome(Outcome, name, odds),
-      winAmount
+      winAmount,
+      Bin(Bin,bins)
     ) where
+
+import Data.Set (Set)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data Outcome = Outcome { name :: String
-  ,odds :: Integer } deriving (Show, Eq)
+  ,odds :: Integer } deriving (Show, Eq, Ord)
+
+data Bin = Bin { bins :: Set Outcome } deriving (Show, Eq)
 
 winAmount :: Outcome -> Integer -> Integer
 winAmount outcome wager = wager * odds outcome
